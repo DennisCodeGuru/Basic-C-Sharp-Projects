c1d815e57e0a6bffdcd7c24703c35f12304e95ce
DennisCodeGuru <denniskalmykovv@gmail.com>
2022-03-08 12:25:03 -0800

    str

---
+++ STRING ASSIGNMENT/STRING ASSIGNMENT/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STRING_ASSIGNMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Dennis";
+            //string quote = "The man said, \\Hello\", Dennis. \n \t Hello on a tab";
+            //string fileName = @"C:\Users\denni";
+
+            //bool trueOrFalse = name.Contains("s");
+            //trueOrFalse = name.EndsWith("s");
+
+            //int lengh = name.Length;
+
+            ////name = name.ToUpper();
+
+            //name = name.ToLower();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Dennis")
+
+            Console.WriteLine(name);
+            Console.ReadLine();
+        }
+    }
+}

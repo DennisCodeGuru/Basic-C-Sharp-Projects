c80e42218675a98320bc9fe41f7aed02c17f8ce0
DennisCodeGuru <denniskalmykovv@gmail.com>
2022-03-10 15:58:05 -0800

    step 280

---
+++ method/method/myMath.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method
+{
+    public class myMath
+    {
+        public int MultiplicationMethod(int inputOne, int inputTwo = 2)
+        {
+            int multiplyAnswer = inputTwo * inputOne;
+            return multiplyAnswer;
+        }
+    }
+}

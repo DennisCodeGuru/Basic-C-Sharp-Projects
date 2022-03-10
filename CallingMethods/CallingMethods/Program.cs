0f18473514402125aaa0b2a14afe0f17ff7044e6
DennisCodeGuru <denniskalmykovv@gmail.com>
2022-03-10 13:38:08 -0800

    step 275

---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User, what number do you want to do math operations on?");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(math.myNum + " + " + userNum + " = " + math.Addition(userNum));
+            Console.WriteLine(math.myNum + " - " + userNum + " = " + math.Subtraction(userNum));
+            Console.WriteLine(math.myNum + " x " + userNum + " = " + math.Multiplication(userNum));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class math
+    {
+        public static int myNum = 50;
+
+        public static int Multiplication(int userNum)
+        {
+            return myNum * userNum;
+        }
+
+        public static int Subtraction(int userNum)
+        {
+            return myNum - userNum;
+        }
+
+        public static int Addition(int userNum)
+        {
+            return myNum + userNum;
+        }
+    }
+}

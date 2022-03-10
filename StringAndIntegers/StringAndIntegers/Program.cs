c6fad21dcc88568cc56840911f38298f6e735ad2
DennisCodeGuru <denniskalmykovv@gmail.com>
2022-03-09 16:17:18 -0800

    StringAndIntegersAssigment

---
+++ StringAndIntegers/StringAndIntegers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAndIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> numbers = new List<int> { 73, 175, 295, 396, 498, 507 };
+                Console.WriteLine("Enter a number to divide by:");
+                int inputNum = Convert.ToInt32(Console.ReadLine());
+
+                foreach (int item in numbers)
+                {
+                    int divideNum = item / inputNum;
+                    Console.WriteLine(item + " divided by " + inputNum + " = " + divideNum);
+                }
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a whole number.");
+            }
+
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Please do not divide by 0.");
+            }
+
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}

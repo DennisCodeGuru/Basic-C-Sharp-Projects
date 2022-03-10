de94e492ca7bc68db685a27315bfa1f84b2ad068
DennisCodeGuru <denniskalmykovv@gmail.com>
2022-03-10 14:47:09 -0800

    step 279

---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input integer:");
+            int intInput = Convert.ToInt32(Console.ReadLine());
+
+            myMath myClass = new myMath();
+            Console.WriteLine(myClass.AdditionMethod(intInput));
+
+            decimal myDecimal = 15.7m;
+            myMath myClass2 = new myMath();
+            Console.WriteLine(myClass2.AdditionMethod(myDecimal));
+
+            string strNum = "42";
+            myMath myClass3 = new myMath();
+            Console.WriteLine(myClass3.AdditionMethod(strNum));
+        }
+    }
+}

38fc47f1882bdcf626d67e81887639384a7e1ddf
DennisCodeGuru <denniskalmykovv@gmail.com>
2022-03-11 13:36:58 -0800

    step 285

---
+++ classSubmission/classSubmission/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classSubmission
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a whole number:");
+            int numInput = Convert.ToInt32(Console.ReadLine());
+
+            int numOutput;
+            myMath myClass = new myMath();
+            myClass.DivideMethod(numInput, out numOutput);
+            Console.WriteLine(numOutput);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ classSubmission/classSubmission/myMath.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classSubmission
+{
+    public class myMath
+    {
+        public void DivideMethod(int numInput)
+        {
+            int divideByTwo = numInput / 2;
+            Console.WriteLine(divideByTwo);
+        }
+        
+        public void DivideMethod(int numInput, out int numOutput)
+        {
+            int divideByTwo = numInput / 2;
+            numOutput = divideByTwo;
+        }
+
+        public void DivideMethod(decimal numInput)
+        {
+            decimal divideByTwo = numInput / 2;
+            Console.WriteLine(divideByTwo);
+        }
+    }
+
+    public static class myStaticMath
+    {
+        public static void DivideMethod(int numInput)
+        {
+            int divideByTwo = numInput / 2;
+            Console.WriteLine(divideByTwo);
+        }
+
+        public static void DivideMethod(int numInput, out int numOutput)
+        {
+            int divideByTwo = numInput / 2;
+            numOutput = divideByTwo;
+        }
+
+        public static void DivideMethod(decimal numInput)
+        {
+            decimal divideByTwo = numInput / 2;
+            Console.WriteLine(divideByTwo);
+        }
+    }
+}

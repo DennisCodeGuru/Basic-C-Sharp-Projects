ffc063cb6ffc387c5898ea9b95ae959597272797
DennisCodeGuru <denniskalmykovv@gmail.com>
2022-03-10 16:11:52 -0800

    step 281

---
+++ methodClass/methodClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodClass
+{
+    public class Program
+    {
+        public static int numOne = 1995;
+        public static int numTwo = 2022;
+        static void Main(string[] args)
+        {
+            myMath myClass = new myMath();
+            myClass.SubtractionMethod(18, 22);
+            myClass.SubtractionMethod(numOne, numTwo);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ methodClass/methodClass/myMath.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodClass
+{
+    public class myMath
+    {
+        public void SubtractionMethod (int numOne, int numTwo)
+        {
+            int subtractNumOne = numTwo - numOne;
+            Console.WriteLine(numTwo);
+        }
+    }
+}

7fc25f7be03e7609222ada66678cec3fd2b09769
DennisCodeGuru <denniskalmykovv@gmail.com>
2022-03-14 15:15:30 -0700

    step 313

---
+++ OperAssignment/OperAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee
+            {
+                firstName = "James",
+                lastName = "Bond"
+            };
+            employee.ListName();
+
+            Employee employee2 = new Employee
+            {
+                firstName = "Steve",
+                lastName = "Rogers"
+            };
+            employee2.ListName();
+
+            employee.Id = 1;
+            employee2.Id = 2;
+
+            bool AreTheyEqual = employee == employee2;
+            Console.WriteLine("Are they equal? " + AreTheyEqual);
+
+            employee2.Id = 1;
+
+            AreTheyEqual = employee == employee2;
+            Console.WriteLine("Now, are they equal? " + AreTheyEqual);
+
+            Console.ReadLine();
+        }
+    }
+}

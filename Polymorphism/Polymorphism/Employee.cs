0a73072fd8453cb7dd080fd58a8200ca8d97bf58
DennisCodeGuru <denniskalmykovv@gmail.com>
2022-03-14 12:40:15 -0700

    step 306

---
+++ Polymorphism/Polymorphism/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine(firstName + lastName);
+        }
+
+        public void Quittable(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("Employee Quit");
+        }
+    }
+}
---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee
+            {
+                firstName = "Sample",
+                lastName = "Student"
+            };
+            employee.SayName();
+
+            IQuittable polyEmployee = new Employee();
+            polyEmployee.Quit();
+
+            Console.ReadLine();
+        }
+    }
+}

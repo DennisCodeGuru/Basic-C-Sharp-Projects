2a8d00a1578480f1a6fa2793d926f30b51f9dd95
DennisCodeGuru <denniskalmykovv@gmail.com>
2022-03-15 12:40:57 -0700

    step 337

---
+++ lambdaSubmission/lambdaSubmission/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lambdaSubmission
+{
+    public class Employee
+    {
+        /// <summary>
+        /// constructor: makes an object of class
+        /// </summary>
+        public Employee()
+        {
+
+        }
+
+        /// <summary>
+        /// constructor: takes firstName, lastName and id as input parameters, returns object with
+        /// the properties set
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="id"></param>
+        public Employee(string firstName, string lastName, int id)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Id = id;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+    }
+}

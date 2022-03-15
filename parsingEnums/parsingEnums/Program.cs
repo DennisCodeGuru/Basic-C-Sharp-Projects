ef3770a7245f6b69dea310193b281be532fe4336
DennisCodeGuru <denniskalmykovv@gmail.com>
2022-03-14 18:29:03 -0700

    step 324

---
+++ parsingEnums/parsingEnums/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parsingEnums
+{
+    public enum DaysOfTheWeek
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the current day of the week: ");
+            bool askAgain = false;
+
+            do
+            {
+                string dayOfTheWeekInput = Console.ReadLine();
+
+                DaysOfTheWeek dayOfTheWeek;
+
+                try
+                {
+                    askAgain = false;
+                    dayOfTheWeek = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), dayOfTheWeekInput.FirstCharToUpper());
+                }
+                catch (Exception ex)
+                {
+                    askAgain = true;
+                    Console.WriteLine("Please enter an actual day of the week.");
+                }
+            } while (askAgain);
+
+            Console.ReadLine();
+        }
+    }
+}

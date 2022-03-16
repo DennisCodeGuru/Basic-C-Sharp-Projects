a5e39d27403c0de0b26c7701505ac0802078eda1
DennisCodeGuru <denniskalmykovv@gmail.com>
2022-03-15 20:07:16 -0700

    step 369

---
+++ dateTimeAssignment/dateTimeAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dateTimeAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = DateTime.Now;
+            Console.WriteLine("Current time: " + dt.ToString());
+
+            Console.WriteLine("\nEnter a number of hours: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            DateTime modifiedDateTime = dt.AddHours(x);
+
+            Console.WriteLine("\nIn " + x + " hours: " + modifiedDateTime.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
